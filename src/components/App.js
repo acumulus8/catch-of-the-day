a1c312febc7ffaf91d2111822957f806bae878b2
Tim <timwilburn8@gmail.com>
2018-05-30 00:55:43 -0400

    added bi-directional data flow and state updating with the editFishForm component, fixed name issue with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,103 @@
-import React from 'react';
-import Header from './Header';
-import Inventory from './Inventory';
-import Order from './Order';
-import sampleFishes from '../sample-fishes';
-import Fish from './Fish';
+import React from "react";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            fishes: {},
-            order: {}
-        }
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
     };
+  }
 
-    addFish = fish => {
-        //1. Take a copy of the existing state (don't want to edit state directly)
-        const fishes = {...this.state.fishes};
-        //2. Add our new fish to that fishes variable
-        fishes[`fish${Date.now()}`] = fish;
-        //3. Set the new fishes objest to state
-        this.setState({
-            fishes: fishes
-        });
-    };
-
-    loadSampleFishes = () => {
-        this.setState({ fishes: sampleFishes });
-    };
-
-    addToOrder = key => {
-        //1. take a copy of state
-        const order = {...this.state.order};
-        //2. either add to the order or update the number in our order
-        order[key] = order[key] + 1 || 1;
-        //3. call setState to update our state object
-        this.setState({ order });
+  componentDidMount() {
+    const { params } = this.props.match;
+    //first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
     }
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
 
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />
-                    <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => 
-                        <Fish 
-                            key={key} 
-                            details={this.state.fishes[key]}
-                            index={key}
-                            addToOrder={this.addToOrder}
-                        />
-                    )}
-                    </ul>
-                </div>
-                <Order 
-                    fishes={this.state.fishes}
-                    order={this.state.order}
-                />
-                <Inventory 
-                    addFish={this.addFish} 
-                    loadSampleFishes={this.loadSampleFishes} 
-                />
-            </div>  
-        )
-    };
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  addFish = fish => {
+    //1. Take a copy of the existing state (don't want to edit state directly)
+    const fishes = { ...this.state.fishes };
+    //2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes objest to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+
+  updateFish = (key, updatedFish) => {
+    // 1. take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. update that state
+    fishes[key] = updatedFish;
+    // 3. set that to state
+    this.setState({ fishes: fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    //1. take a copy of state
+    const order = { ...this.state.order };
+    //2. either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    //3. call setState to update our state object
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                index={key}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          addFish={this.addFish}
+          updateFish={this.updateFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
