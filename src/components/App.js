5abab4c9901e8ebe380549cdc3068634dae73344
Tim <timwilburn8@gmail.com>
2018-06-01 16:37:49 -0400

    added delete from inventory and delete from order functionality: deleting state

--- src/components/App.js
+++ src/components/App.js
@@ -59,6 +59,15 @@ class App extends React.Component {
     this.setState({ fishes: fishes });
   };
 
+  deleteFish = key => {
+    //1. take a copy of state
+    const fishes = { ...this.state.fishes };
+    //2. update the state
+    fishes[key] = null;
+    //3. set the new state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -68,6 +77,15 @@ class App extends React.Component {
     const order = { ...this.state.order };
     //2. either add to the order or update the number in our order
     order[key] = order[key] + 1 || 1;
+    //3. call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = key => {
+    //1. take a copy of state
+    const order = { ...this.state.order };
+    //2. remove from order state
+    delete order[key];
     //3. call setState to update our state object
     this.setState({ order });
   };
@@ -88,10 +106,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
